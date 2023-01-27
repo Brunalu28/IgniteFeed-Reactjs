fdd8e43dfaffd61c6faaf26d3dd64d617f065bed
Your Name <youremail@domain.com>
2023-01-26 23:35:33 -0300

    Componente avatar criado.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,13 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+
 export function Post(){
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/Brunalu28.png" />
+                    <Avatar src="https://github.com/Brunalu28.png" />
                     <div className={styles.authorInfo}>
                         <strong>Luiza Bruna</strong>
                         <span>Web Developer</span>
